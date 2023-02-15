270f2a8f9bd297db6f4eb96859e4f70d1ce95638
solp22 <sol.puentereal@gmail.com>
2023-02-15 09:02:35 -0800

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* *
+/**
  * main - this program prints a message
  *
  * Return: Always 0 (Success)
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
-	return(0)
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
