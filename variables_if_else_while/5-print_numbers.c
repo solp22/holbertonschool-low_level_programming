5212e8d631d2d6b9dbaac6876104b758cadc2f61
solp22 <sol.puentereal@gmail.com>
2023-02-16 05:45:52 -0800

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
