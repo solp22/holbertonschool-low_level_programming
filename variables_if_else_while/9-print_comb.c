24121c41b6c48b82a847407f9ad96f348258c57d
solp22 <sol.puentereal@gmail.com>
2023-02-16 08:51:10 -0800

    combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints combinations of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');	
+		if (ch != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
