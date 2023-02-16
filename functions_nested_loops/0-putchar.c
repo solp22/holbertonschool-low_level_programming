371b043a796f89e28661594bb0ac2c3fb10b58eb
solp22 <sol.puentereal@gmail.com>
2023-02-16 09:30:26 -0800

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - this program prints
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
