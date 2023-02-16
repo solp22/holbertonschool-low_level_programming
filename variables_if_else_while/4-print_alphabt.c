6b0f125267a4845025ed020b5855cd31721d60dd
solp22 <sol.puentereal@gmail.com>
2023-02-16 05:32:54 -0800

    s

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,10 +13,10 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' | ch != 'q')
-			putchar(ch);
-		else
+		if (ch == 'e' | ch == 'q')
 			ch++;
+		else
+			putchar(ch);
 	}
 	putchar('\n');
 	return (0);
