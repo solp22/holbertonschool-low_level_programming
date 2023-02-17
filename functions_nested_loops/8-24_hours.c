963e3820ec9ce90521add74044447a950a1d8db7
solp22 <sol.puentereal@gmail.com>
2023-02-17 08:34:06 -0800

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,23 +11,17 @@
 void jack_bauer(void)
 {
 	int x;
-	int y;
 	int a;
-	int b;
 
 	for (x = 0; x <= 23; x++)
 	{
-		x = x / 10;
-		y = x % 10;
 		for (a = 0; a <= 59; x++)
 		{
-			_putchar (x + '0');
-			_putchar (y + '0');
+			_putchar ((x / 10) + '0');
+			_putchar ((x % 10) + '0');
 			_putchar(':');
-			a = a / 10;
-			b = a % 10;
-			_putchar (a + '0');
-			_putchar (b + '0');
+			_putchar ((a / 10) + '0');
+			_putchar ((a % 10) + '0');
 			_putchar ('\n');
 		}
 	}
