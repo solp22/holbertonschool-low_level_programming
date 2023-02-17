15dadcdf829b901d56825757a87d822b743a9f20
solp22 <sol.puentereal@gmail.com>
2023-02-17 05:37:52 -0800

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - check the code.
+ *
+ * @x: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = x * -1;
+		_putchar (x);
+	}
+	else
+		_putchar (x);
+}
