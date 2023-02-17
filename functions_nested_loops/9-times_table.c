b8e1085c2f6ed01504c8877877b906b23de67d5d
solp22 <sol.puentereal@gmail.com>
2023-02-17 09:13:17 -0800

    timestable

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time tables
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int i;
+	int p;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (p = 0; p <= 9; p++)
+		{
+			r = i * p;
+			if (r >= 10)
+			{
+				_putchar ((r / 10) + '0');
+				_putchar ((r % 10) + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+			else
+			{
+				_putchar (r);
+				_putchar (',');
+				_putchar (' ');
+				_putchar (' ');
+			}
+			_putchar ('\n');
+		}
+	}
+}
