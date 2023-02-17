fdf7dda1a185b5df34867c5ea203bd8807e30b71
solp22 <sol.puentereal@gmail.com>
2023-02-17 06:09:43 -0800

    ld

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ *
+ * @a: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int a)
+{
+	a = a % 10;
+	return (a);
+}
