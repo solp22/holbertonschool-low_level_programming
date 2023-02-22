4e91a2cd496efc193d3e2d8d7b2cedcd3e9c2818
solp22 <sol.puentereal@gmail.com>
2023-02-22 09:10:07 -0800

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ *
+ * @c: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
