99dcfc9871460652bd1ec2ab65beb91a90833d96
solp22 <sol.puentereal@gmail.com>
2023-02-23 08:47:19 -0800

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_triangle - check code
+ *
+ * @size: number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_triangle(int size)
+{
+	int y;
+	int x;
+	int z;
+	
+
+	if (size >= 1)
+	{
+		for (y = 0; y < size; y++)
+		{
+			z = size - y;
+
+			for (x = 1; x <= size; x++)
+			{
+				if (x >= z)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+
+			_putchar ('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
