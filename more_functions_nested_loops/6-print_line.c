fe8231e88f6f6125ff5efe09c9dffa20f4e104eb
solp22 <sol.puentereal@gmail.com>
2023-02-23 04:07:02 -0800

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * @n: numbers
+ * @i: numbers
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n >= 1)
+	{
+		for (i = 0; i <= n; i++)
+		{
+		       _putchar ('_');
+		}
+	}
+	_putchar('\n');
+}
