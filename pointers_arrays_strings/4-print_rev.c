7b27249e05ad033df38b2d97eaa7a0bc6db8e9a2
solp22 <sol.puentereal@gmail.com>
2023-02-27 09:53:28 -0800

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int x, i;
+
+	for (x = 0; s[x]; x++)
+	{
+	}
+	for (i = (x - 1); i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
