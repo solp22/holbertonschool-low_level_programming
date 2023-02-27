8c22b2a83c4223662f9084bd8ae9b6ef9e5bbb96
solp22 <sol.puentereal@gmail.com>
2023-02-27 08:52:39 -0800

    string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - check code
+ *
+ * @s: input
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		count = count + 1;
+	}
+	return (count);
+}
