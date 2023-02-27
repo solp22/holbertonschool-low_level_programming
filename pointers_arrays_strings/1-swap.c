71ff5c263b7ce9a239e854136be908fb51c1c699
solp22 <sol.puentereal@gmail.com>
2023-02-27 06:58:50 -0800

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap - check code
+ *
+ * @a: number
+ * @b: other number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c == *b;
+	*b == *a;
+	*a == **c;
+}
