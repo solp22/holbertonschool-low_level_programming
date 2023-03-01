7081acf6f04ebcf949677f90cc45f7d1c5ffea70
solp22 <sol.puentereal@gmail.com>
2023-03-01 08:54:18 -0800

    compare

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: string
+ * @s2: another string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		return (s1[i] - s2[i]);
+	}
+	return (0);
+}
