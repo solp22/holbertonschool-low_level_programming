7de96a3b1489ed9a1707d83bbd6adccfc74a65a4
solp22 <sol.puentereal@gmail.com>
2023-03-01 10:30:48 -0800

    uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes string to uppercase
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; s++)
+	{
+		if ((s[i] >= 'a') && (s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
