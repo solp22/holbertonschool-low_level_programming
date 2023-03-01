e314832349079dfc26cebbd62ab84d63cf42f1c4
solp22 <sol.puentereal@gmail.com>
2023-03-01 11:58:49 -0800

    a

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -10,9 +10,9 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i]; s++)
+	for (i = 0; s[i]; i++)
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
+		if (s[i] >= 97 && s[i] <= 122)
 		{
 			s[i] = s[i] - 32;
 		}
