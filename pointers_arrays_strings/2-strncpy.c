9976231897a5c2f980fd179ec38899cca59b8a9b
solp22 <sol.puentereal@gmail.com>
2023-03-01 07:07:55 -0800

    fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] && i < n; i++)
 	{
+		if (src[i] == '\0')
+			break;
 		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
