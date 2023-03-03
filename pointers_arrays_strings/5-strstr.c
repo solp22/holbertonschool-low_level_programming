4e849bc53ff6924f0ac9a893d0a665211cbe5082
solp22 <sol.puentereal@gmail.com>
2023-03-03 10:14:42 -0800

    substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locate substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, temp = 0;
+
+	for (i = 0; needle[i]; i++)
+	{
+	}
+	while (*haystack)
+	{
+		if (*haystack == needle[temp])
+			temp++;
+		else
+			temp = 0;
+		if (i == temp)
+			return (haystack - i);
+		haystack++;
+	}
+	return (NULL);
+}
