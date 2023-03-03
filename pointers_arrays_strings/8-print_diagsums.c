e89763be379ed121a20ef488f291354df00badb2
solp22 <sol.puentereal@gmail.com>
2023-03-03 15:12:38 -0800

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,5 +16,5 @@ void print_diagsums(int *a, int size)
 		diag1 = diag1 + a[i + (i * size)];
 		diag2 = diag2 + a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d,%d\n", diag1, diag2);
+	printf("%d, %d\n", diag1, diag2);
 }
