6bed38281ab03c1f4cb1901ef658556b9b0c2e21
solp22 <sol.puentereal@gmail.com>
2023-03-03 04:51:19 -0800

    fill memory

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * _memset - fills n memory with char
  * @s: string
  * @b: char
@@ -11,4 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
