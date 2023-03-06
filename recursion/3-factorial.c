ac71102fbdc28a072d1782870f404740477d05f5
solp22 <sol.puentereal@gmail.com>
2023-03-06 06:39:22 -0800

    factorial recursion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of a number
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
