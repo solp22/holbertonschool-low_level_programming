2a7b29a6f4429133f03b46d34d134a4d9c8d9e2e
solp22 <sol.puentereal@gmail.com>
2023-03-08 05:58:58 -0800

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ * @argc: number
+ * @argv: array of strings
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
