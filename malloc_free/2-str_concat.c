14bec8b863bab79b80e4d954842e1671f8f96e5b
solp22 <sol.puentereal@gmail.com>
2023-03-13 14:51:24 -0700

    concatenate

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: Always 0 (Success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i = 0, x = 0, y, z, sum = i + x;
+
+	for (i = 0; s1[i]; i++)
+	{
+	}
+	for (x = 0; s2[x]; x++)
+	{
+	}
+	p = malloc(sizeof(char) * sum + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (y = 0; s1[y]; y++)
+		{
+			p[y] = s1[y];
+		}
+		for (z = 0; s2[z]; z++, y++)
+		{
+			p[y] = s2[z];
+		}
+	return (p);
+	}
+}
