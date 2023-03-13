abe0a1e08ddfa3db26ac3704ec6339ea8c74936b
solp22 <sol.puentereal@gmail.com>
2023-03-13 10:26:09 -0700

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - check the code
+ * @str: string
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+	int x;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+	p = malloc(sizeof(char) * i);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (x = 0; x < i; x++)
+		{
+			p[x] = str[x];
+		}
+	}
+	return (p);
+}
