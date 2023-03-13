0fe448d2d31adbb113c6da6235b32d20525b7884
solp22 <sol.puentereal@gmail.com>
2023-03-13 09:39:37 -0700

    create array

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -36,12 +37,15 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    char *buffer;
 
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
     simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
+    free(buffer);
     return (0);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C - Hello, World holberton project
+a
