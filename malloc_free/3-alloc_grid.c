427da857989890d20566ce6cad316a68a3e67dcc
solp22 <sol.puentereal@gmail.com>
2023-03-14 10:43:48 -0700

    fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 			array[i] = malloc(sizeof(int) * width);
 			if (array[i] == NULL)
 			{
-				for (--i; i > 0; i--)
+				for (--i; i >= 0; --i)
 				{
 					free(array[i]);
 				}
