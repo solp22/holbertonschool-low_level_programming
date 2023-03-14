4c255cd28de102bad566df5c797081514b711625
solp22 <sol.puentereal@gmail.com>
2023-03-14 10:18:45 -0700

    grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - makes a  grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: Always 0 (Success)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array, i, x;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(int*) * height);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			array[i] = malloc(sizeof(int) * width);
+			for (x = 0; x < width; x++)
+			{
+				array[i][x] = 0;
+			}
+		}
+	return (array);
+	}
+}	
