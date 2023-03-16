04730e7ff074618a501884173490fce12a3fc515
solp22 <sol.puentereal@gmail.com>
2023-03-16 07:52:58 -0700

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * _calloc - allocates memory
+ * @nmemb: elements of array
+ * @size: size of bytes of nmemb
+ * Return: Always 0 (Success)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *pointer;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < (nmemb * size); i++)
+		{
+			pointer[i] = 0;
+		}
+	}
+	return (pointer);
+}
