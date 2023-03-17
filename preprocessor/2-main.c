5571aa5c2e73ad3cb170f027ddafef74a15af5f0
solp22 <sol.puentereal@gmail.com>
2023-03-17 09:53:37 -0700

    file name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef __FILE__
+#define __FILE__
+#endif
+
+/**
+ * main - prints name of program
+ * Return: Always 0 (Success)
+ */
+
+int main ()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
