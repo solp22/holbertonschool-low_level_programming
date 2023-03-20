1286e0ab47ca344db2db2bd39cd937f8904e7173
solp22 <sol.puentereal@gmail.com>
2023-03-20 06:58:02 -0700

    fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-typedef struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure with name age and owner of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ */
+
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 } my_dog;
+
+#endif
