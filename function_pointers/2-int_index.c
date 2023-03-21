e41f47b41442b7c7dd7ba29b408960243f93c8ad
solp22 <sol.puentereal@gmail.com>
2023-03-21 09:16:34 -0700

    fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -7,6 +7,7 @@
  * @array: array
  * @size: size of array
  * @cmp: pointer to function that compares values
+ * Return: Always 0 (Success)
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -15,11 +16,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (*cmp != NULL)
 	{
-		(*cmp)(array[i]);
-		if ((*cmp)(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (array != NULL)
+			{
+				(*cmp)(array[i]);
+				if ((*cmp)(array[i]) != 0)
+					return (i);
+			}
+		}
 	}
 	return (-1);
 }
