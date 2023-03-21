22a765ef8e516bb23a571519a2011bb9977a38a9
solp22 <sol.puentereal@gmail.com>
2023-03-21 07:22:25 -0700

    array iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,15 +3,21 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes function given each element of an array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (*action != NULL)
 	{
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
 	}
 }
