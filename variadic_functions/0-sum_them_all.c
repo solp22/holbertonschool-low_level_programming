0f91555a02ba73d3364a5a83d3032a1ba8c9b701
solp22 <sol.puentereal@gmail.com>
2023-03-23 07:43:47 -0700

    var func sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,15 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-
 	va_list args;
+
 	va_start(args, n);
-	
 	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
 		{
 			int x = va_arg(args, int);
+
 			sum = sum + x;
 		}
 		va_end(args);
