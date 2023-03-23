81511af01334e7c590cbb29309bc77d6b0dc590e
solp22 <sol.puentereal@gmail.com>
2023-03-23 09:44:20 -0700

    print nums

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,10 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	
+
 	if (n != 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < (n - 1); i++)
 		{
 			if (separator != NULL)
 				printf("%d%s", va_arg(args, int), separator);
