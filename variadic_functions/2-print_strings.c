ab6dd4d5fd6bea0c6fa3fbc5d0d97d2ba0f81c52
solp22 <sol.puentereal@gmail.com>
2023-03-23 15:47:10 -0700

    fix

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -30,7 +30,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 				printf("%s", va_arg(args, char *));
 		}
-		printf("%s", va_arg(args, char *));
+		if (va_arg(args, char *) != NULL)
+			printf("%s", va_arg(args, char *));
+		else
+			printf("(nil)");
 
 		va_end(args);
 	}
