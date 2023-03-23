87101d57ca34e79e90977c482463dce057757b6b
solp22 <sol.puentereal@gmail.com>
2023-03-23 05:28:17 -0700

    calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,20 +9,24 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	
+	int i;
 
-    while (ops[i].op)
-    {
-	if (strcmp (s, ops[i].op) == 0)
-		return (ops[i].f);
-	i++;
-    }
-    return (NULL);
+	while (i < 4)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			printf("%s\n", ops[i].op);
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,21 +10,24 @@
 int main(int argc, char *argv[])
 {
 	int (*pointer)(int, int);
+	int num1, num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argc == 4)
+	else
 	{
-		if ((strcmp(argv[2], "/") == 0) || ((strcmp(argv[2], "%") == 0) && (atoi(argv[3]) == 0)))
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);	
+		if ((strcmp(argv[2], "/") == 0) || ((strcmp(argv[2], "%") == 0) && (num2 == 0)))
 		{
 			printf("Error\n");
 			exit(100);
 		}
 		pointer = get_op_func(argv[2]);
-		printf("%d\n", pointer(atoi(argv[1]), atoi(argv[3])));
+		printf("%d\n", pointer(num1, num2));
 		return (0);
 	}
 	printf("Error\n");
