273fb0ba2762df60d7a2343d5443ba47c00eba43
solp22 <sol.puentereal@gmail.com>
2023-03-23 15:55:07 -0700

    fix

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,14 +22,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			if (separator != NULL)
 			{
-				if (args != NULL)
+				if (va_arg(args, char *)` != NULL)
 					printf("%s%s", va_arg(args, char *), separator);
 				else
 					printf("(nil)%s", separator);
 			}
 			else
 			{
-				if (args != NULL)
+				if (va_arg(args, char *) != NULL)
 					printf("%s", va_arg(args, char *));
 				else
 					printf("(nil)");
