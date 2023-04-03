c73be49c05d2779ce938a786c16b5e6cb4fee4f7
solp22 <sol.puentereal@gmail.com>
2023-04-03 07:06:57 -0700

    print elements in linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of linked list
+ * @h: 
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{	
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
