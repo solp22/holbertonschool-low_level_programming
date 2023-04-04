531fdcac35911199e7d77dc5862fc09fa3dae716
solp22 <sol.puentereal@gmail.com>
2023-04-04 11:29:03 -0700

    insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = NULL;
 			new->next = (*h);
 			(*h)->prev = new;
-			(*h) = new
+			(*h) = new;
 		}
 		else
 		{
