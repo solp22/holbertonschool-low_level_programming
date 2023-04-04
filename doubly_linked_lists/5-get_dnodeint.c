f0874d65505e569f155d8f0c0d8ca0ac845488f8
solp22 <sol.puentereal@gmail.com>
2023-04-04 10:25:58 -0700

    nth node betty fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (count == index)
-                        return (head);
+			return (head);
 		count++;
 		head = head->next;
 	}
