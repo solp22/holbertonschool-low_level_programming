6deb8992e5f83e1c288fac1e77fab7004564a66a
solp22 <sol.puentereal@gmail.com>
2023-04-04 06:37:48 -0700

    add node at beginning fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,10 +14,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new->n = n;
-	new->next = (*head);
-	new->prev = NULL;
-	(*head) = new;
+	if ((*head == NULL))
+	{
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+		(*head) = new;
+	}
+	else
+	{
+		new->n = n;
+		new->prev = NULL;
+		new->next = (*head);
+		(*head)->prev = new;
+		(*head) = new;
+	}
 	return (*head);
 }
