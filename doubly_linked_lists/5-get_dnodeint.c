947cb30dbe7780c7f702e3f1abb01c51aa5f4d01
solp22 <sol.puentereal@gmail.com>
2023-04-04 10:24:00 -0700

    nth node fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,10 +13,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
+		if (count == index)
+                        return (head);
 		count++;
 		head = head->next;
-		if (count == index)
-			return (head);
 	}
 	return (NULL);
 }
