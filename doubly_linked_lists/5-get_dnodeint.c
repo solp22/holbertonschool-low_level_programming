acb4032ddc085e3ab0072a1bdf6ce0e36d80c6f5
solp22 <sol.puentereal@gmail.com>
2023-04-04 10:21:46 -0700

    nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of list
+ * @head: pointer to first node
+ * @index: index fo the node
+ * Return: address of element
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		count++;
+		head = head->next;
+		if (count == index)
+			return (head);
+	}
+	return (NULL);
+}
