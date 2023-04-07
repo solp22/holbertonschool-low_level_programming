f31fcb950553fdb81c007f9ea2ea73b79ae584ad
solp22 <sol.puentereal@gmail.com>
2023-04-07 14:28:16 -0700

    insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -46,8 +46,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (*h);
 	}
 	else if (idx == size)
+	{
 		add_dnodeint_end(h, n);
 		return (*h);
+	}
 	else
 	{
 		newnode->n = n;
