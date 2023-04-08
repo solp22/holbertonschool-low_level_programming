3ec20e2e045c7a7a89d2ec44796200d736c11b78
solp22 <sol.puentereal@gmail.com>
2023-04-08 11:31:40 -0700

    delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,20 @@
 #include "lists.h"
+/**
+ * calcsize - calculates size of node
+ * @node: pointer to node
+ * Return: size of node
+ */
+unsigned int calcsize(dlistint_t *node)
+{
+	unsigned int size = 0;
+
+	while (node != NULL)
+	{
+		node = node->next;
+		size++;
+	}
+	return (size);
+}
 
 /**
  * delete_dnodeint_at_index - deletes node at index of list
@@ -10,12 +26,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *del = *head;
 	unsigned int i = 0;
+	unsigned int size = calcsize(*head);
 
 	while (i < index)
 	{
 		del = del->next;
 		i++;
 	}
+	if (size < index)
+		return (-1);
 	if (*head == NULL || del == NULL)
 		return (-1);
 	if (*head == del)
