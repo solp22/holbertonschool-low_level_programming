e3b7f35df0d9c478c2c47e006264632a19a4c3d1
solp22 <sol.puentereal@gmail.com>
2023-04-11 10:30:47 -0700

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,11 +11,12 @@ int create_file(const char *filename, char *text_content)
 	int fptr;
 
 	if (filename == NULL)
-	       return (-1);	
-	fptr = open(filename, O_WRONLY | O_CREAT, 0600);
-	if (!fptr)
 		return (-1);
-	dprintf(fptr, "%s", text_content);
+	fptr = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fptr == -1)
+		return (-1);
+	if (text_content != NULL)
+		dprintf(fptr, "%s", text_content);
 	close(fptr);
 	return (1);
 }
