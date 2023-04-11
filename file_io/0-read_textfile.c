3cf78be9b376021065ad5e7a9d0303777e56c767
solp22 <sol.puentereal@gmail.com>
2023-04-11 09:04:17 -0700

    fix?

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,15 +12,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char letter;
 	unsigned int count = 0;
 	FILE *fptr;
-	
+
 	fptr = fopen(filename, "r");
 	if (fptr == NULL)
 		return (0);
 	letter = fgetc(fptr);
 	while (letters > count && letter != EOF)
 	{
-		write(1, &letter, 1);
-		letter = fgetc(fptr);	
+		write(STDOUT_FILENO, &letter, 1);
+		letter = fgetc(fptr);
 		count++;
 	}
 	fclose(fptr);
