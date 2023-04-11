4903707dcbd2a1d1b112069d9027e60ac26a820e
solp22 <sol.puentereal@gmail.com>
2023-04-11 10:18:32 -0700

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of file
+ * @text_content: string with content to put on file
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fptr;
+
+	if (filename == NULL)
+	       return (-1);	
+	fptr = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (!fptr)
+		return (-1);
+	dprintf(fptr, "%s", text_content);
+	close(fptr);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stddef.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
