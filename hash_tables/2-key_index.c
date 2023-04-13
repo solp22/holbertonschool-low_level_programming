092cd00b9edc81ba4c903c16342dc0748196e590
solp22 <sol.puentereal@gmail.com>
2023-04-13 11:40:35 -0700

    index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ * @key: the key 
+ * @size: the size of the array of the hash table
+ * Return: index where the key is stored in array
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2((unsigned char *)key) % size);
+}
