2466f331c24d0a5ffcafb5c0aab4face5cc0cae7
solp22 <sol.puentereal@gmail.com>
2023-04-14 10:02:02 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,7 +24,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	if (ht->array[index] == NULL)
 	{
-		new->next = ht->array[index];
 		ht->array[index] = new;
 	}
 	else
@@ -39,9 +38,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(new->key);
 				free(new->value);
 				free(new);
+				return (0);
 			}
+			temp = temp->next;
 		}
+		new->next = ht->array[index];
+		ht->array[index] = new
 	}
 
 	return (1);
-}		 
+}
