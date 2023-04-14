93b79a6c21b2d007c4465dd13aaf884ab21df400
solp22 <sol.puentereal@gmail.com>
2023-04-14 09:35:14 -0700

    add to table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ * Return: 1 if success, 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (ht->array[index] == NULL)
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+	else
+	{
+		temp = ht->array[index];
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value);
+				free(new->key);
+				free(new->value);
+				free(new);
+			}
+		}
+	}
+
+	return (1);
+}		 
