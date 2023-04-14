6bd8cc7f2a819661dde5387324eafc673035acfc
solp22 <sol.puentereal@gmail.com>
2023-04-14 10:04:13 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp = temp->next;
 		}
 		new->next = ht->array[index];
-		ht->array[index] = new
+		ht->array[index] = new;
 	}
 
 	return (1);
