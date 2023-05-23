c56c2bbcc1b6ac5e062f4fdb9ed4907fc257116f
solp22 <sol.puentereal@gmail.com>
2023-05-23 11:25:44 -0700

    fix betty + print error

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,16 +8,16 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (!array)
-        return (-1);
-    
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array [%d]\n", array[i]);
-        if (array[i] == value)
-           return (i);
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
