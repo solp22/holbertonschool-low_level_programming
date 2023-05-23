83184dce9f1430213907642713af6dba5cfd1141
solp22 <sol.puentereal@gmail.com>
2023-05-23 11:22:05 -0700

    add linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for value in an array using linear search
+ * @array:  pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index or -1 in case of failure
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+
+    if (!array)
+        return (-1);
+    
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array [%d]\n", array[i]);
+        if (array[i] == value)
+           return (i);
+    }
+    return (-1);
+}
